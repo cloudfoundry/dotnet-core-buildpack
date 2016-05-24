07893a7826274cd6d867576be6f0133dbcd7b692
James Wen <jrw2175@columbia.edu>
2016-05-24 14:42:00 -0400

    Add offline compatibility for cached buildpack
    - Add dotnet dependency to manifest
    - Have DotnetInstaller use compile-extensions offline dependency
      functionality
    - Add buildpack cache support for Dotnet (no installation between
      same app stages)
    
    Signed-off-by: Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
    Signed-off-by: Dave Goddard <dave@goddard.id.au>
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- cf_spec/integration/deploy_aspnet5_app_spec.rb
+++ cf_spec/integration/deploy_aspnet5_app_spec.rb
@@ -50,4 +50,17 @@ describe 'CF Asp.Net5 Buildpack' do
       expect(browser).to have_header('application/json; charset=utf-8')
     end
   end
+
+  context 'deploying simple vendored web app with no internet', :cached do
+    let(:app_name) { 'asp_web_app_vendored' }
+
+    it 'displays a simple text homepage' do
+      expect(app).to be_running
+      expect(app).to have_logged /ASP.NET Core buildpack is done creating the droplet/
+      expect(app).not_to have_internet_traffic
+
+      browser.visit_path('/')
+      expect(browser).to have_body('Hello World!')
+    end
+  end
 end
