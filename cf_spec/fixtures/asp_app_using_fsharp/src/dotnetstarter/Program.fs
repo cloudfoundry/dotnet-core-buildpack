d5e911eee8382fbe2bdf25339554383ca4c30cf0
Anna Thornton <athornton@pivotal.io>
2016-12-12 18:01:46 -0500

    Add .NET Core F# app fixture and spec
    [#135725073]
    
    Signed-off-by: Anna Thornton <athornton@pivotal.io>
    Signed-off-by: Keaty <kgross@pivotal.io>

---
+++ cf_spec/fixtures/asp_app_using_fsharp/src/dotnetstarter/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Configuration
+
+type Startup() =
+    member this.Configure(app: IApplicationBuilder) = 
+      app.UseDefaultFiles() |>ignore
+      app.UseStaticFiles() |>ignore
+
+
+[<EntryPoint>]
+let main argv = 
+    let config = ConfigurationBuilder().AddCommandLine(argv).Build()
+
+    let host = WebHostBuilder().UseKestrel().UseConfiguration(config).UseStartup<Startup>().Build()
+    host.Run()
+    0
---
+++ cf_spec/integration/deploy_asp_app_using_fsharp_spec.rb
@@ -0,0 +1,23 @@
+$LOAD_PATH << 'cf_spec'
+require 'spec_helper'
+
+describe 'CF ASP.NET Core Buildpack' do
+  subject(:app) { Machete.deploy_app(app_name) }
+  let(:browser) { Machete::Browser.new(app) }
+
+  after do
+    Machete::CF::DeleteApp.new.execute(app)
+  end
+
+  context 'deploying simple web app written in F#' do
+    let(:app_name) { 'asp_app_using_fsharp' }
+
+    it 'displays a simple text homepage' do
+      expect(app).to be_running
+      expect(app).to have_logged(/ASP.NET Core buildpack is done creating the droplet/)
+
+      browser.visit_path('/')
+      expect(browser).to have_body('Hello World from F#!')
+    end
+  end
+end
