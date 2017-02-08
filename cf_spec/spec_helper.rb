abe9f422a2ca05d690e029f5edf5cb632c14100b
Sam Smith <sesmith177@gmail.com>
2017-02-08 09:47:35 -0500

    Retry failed specs
    - for flakiness in CI

--- cf_spec/spec_helper.rb
+++ cf_spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'bundler/setup'
 require 'machete'
 require 'machete/matchers'
+require 'rspec/retry'
 require_relative '../lib/buildpack.rb'
 require_relative '../lib/buildpack/shell.rb'
 require_relative '../lib/buildpack/sdk_info.rb'
@@ -11,6 +12,9 @@ Machete.logger = Machete::Logger.new('log/integration.log')
 RSpec.configure do |config|
   config.color = true
   config.tty = true
+  config.verbose_retry = true
+  config.default_retry_count = 3
+  config.default_sleep_interval = 5
 
   config.filter_run_excluding cached: ENV['BUILDPACK_MODE'] == 'uncached'
   config.filter_run_excluding uncached: ENV['BUILDPACK_MODE'] == 'cached'
