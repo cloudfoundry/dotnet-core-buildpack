a008265fa625c16c30776c36462803e13bd88eb6
Dave Goddard <dave@goddard.id.au>
2018-01-12 10:49:01 -0400

    Set GOOS=linux during buildpack build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ set -exuo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 source .envrc
 
-go build -ldflags="-s -w" -o bin/supply dotnetcore/supply/cli
-go build -ldflags="-s -w" -o bin/finalize dotnetcore/finalize/cli
+GOOS=linux go build -ldflags="-s -w" -o bin/supply dotnetcore/supply/cli
+GOOS=linux go build -ldflags="-s -w" -o bin/finalize dotnetcore/finalize/cli
