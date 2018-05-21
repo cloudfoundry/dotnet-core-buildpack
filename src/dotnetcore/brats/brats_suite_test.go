6690a41dc7fb25fc9629b6b1c94d44b5c2e6dc34
Sam Coward <scoward@pivotal.io>
2018-05-21 00:30:25 -0400

    Don't set stack when packaging buildpack yet

--- src/dotnetcore/brats/brats_suite_test.go
+++ src/dotnetcore/brats/brats_suite_test.go
@@ -26,7 +26,7 @@ func init() {
 
 var _ = SynchronizedBeforeSuite(func() []byte {
 	// Run once
-	return bratshelper.InitBpData("cflinuxfs2").Marshal()
+	return bratshelper.InitBpData("").Marshal()
 }, func(data []byte) {
 	// Run on all nodes
 	bratshelper.Data.Unmarshal(data)
