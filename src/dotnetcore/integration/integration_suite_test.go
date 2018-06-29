0137d1e759269227a7f398e895ec63168f8663a2
Tyler Phelan <tphelan@pivotal.io>
2018-06-29 09:49:40 -0400

    Fail integration tests if buildpack is over 1G
    [#158696537]

--- src/dotnetcore/integration/integration_suite_test.go
+++ src/dotnetcore/integration/integration_suite_test.go
@@ -42,6 +42,12 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 		packagedBuildpack, err := cutlass.PackageUniquelyVersionedBuildpack(os.Getenv("CF_STACK"))
 		Expect(err).NotTo(HaveOccurred())
 
+		buildpackFile, err := os.Open(packagedBuildpack.File) // For read access.
+		Expect(err).NotTo(HaveOccurred())
+		buildpackFileStat, err := buildpackFile.Stat()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(buildpackFileStat.Size() < 1024*1024*1024).To(BeTrue(), "Buildpack file size must be less than 1G")
+
 		data, err := json.Marshal(packagedBuildpack)
 		Expect(err).NotTo(HaveOccurred())
 		return data
