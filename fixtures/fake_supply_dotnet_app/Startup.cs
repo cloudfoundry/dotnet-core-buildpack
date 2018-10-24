c5cdd373c7e6a932e31eef8d0c7702430f6c6a3e
Danny Joyce <djoyce@pivotal.io>
2018-10-24 10:45:25 -0400

    adds dotnet test and fixture from depreciated multibuildpack
    [#149434941]
    
    Co-authored-by: Daniel Thornton <dthornton@pivotal.io>

---
+++ fixtures/fake_supply_dotnet_app/Startup.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
+
+namespace HelloWeb
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context =>
+            {
+                var bosh2 = new Process();
+                bosh2.StartInfo.FileName = "bosh2";
+                bosh2.StartInfo.Arguments = "--version";
+                bosh2.StartInfo.RedirectStandardOutput = true;
+                bosh2.Start();    
+                
+                var output = bosh2.StandardOutput.ReadToEnd();
+                bosh2.WaitForExit();
+                return context.Response.WriteAsync("bosh2: " + output);
+            });
+        }
+    }
+}
---
+++ src/dotnetcore/integration/fake_supply_before_dotnet_test.go
@@ -0,0 +1,38 @@
+package integration_test
+
+import (
+	"path/filepath"
+
+	"github.com/cloudfoundry/libbuildpack/cutlass"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("running supply buildpacks before the dotnet-core buildpack", func() {
+	var app *cutlass.App
+	AfterEach(func() {
+		if app != nil {
+			app.Destroy()
+		}
+		app = nil
+	})
+
+	Context("the app is pushed", func() {
+		BeforeEach(func() {
+			app = cutlass.New(filepath.Join(bpDir, "fixtures", "fake_supply_dotnet_app"))
+			app.Buildpacks = []string{
+				"https://buildpacks.cloudfoundry.org/fixtures/new_supply_bosh2.zip",
+				"dotnet_core_buildpack",
+			}
+		})
+
+		It("finds the supplied dependency in the runtime container", func() {
+			PushAppAndConfirm(app)
+
+			Expect(app.Stdout.String()).To(ContainSubstring("SUPPLYING BOSH2"))
+
+			Expect(app.GetBody("/")).To(MatchRegexp("bosh2: version 2.0.1-74fad57"))
+		})
+	})
+})
