bda23cedf8c839f7dda0a0a6ab4e1b8d7a887516
Zander Mackie <amackie@pivotal.io>
2019-09-24 17:04:13 -0400

    Remove cflinuxfs2 dependencies
    Fixes #214
    
    [#166710429]
    [#168706472]

--- src/dotnetcore/finalize/finalize.go
+++ src/dotnetcore/finalize/finalize.go
@@ -16,9 +16,7 @@ import (
 )
 
 var cfStackToOS = map[string]string{
-	"cflinuxfs2":  "ubuntu.14.04-x64",
-	"cflinuxfs3":  "ubuntu.18.04-x64",
-	"cflinuxfs3m": "ubuntu.18.04-x64",
+	"cflinuxfs3": "ubuntu.18.04-x64",
 }
 
 type Project interface {
