9badaa3858c143c6eedf5380cfa05740b8410c15
Arjun Sreedharan <asreedharan@vmware.com>
2022-08-04 17:59:16 +0000

    update angular_dotnet app from 3.x -> 6.x
    This is a continuation of prev commit but
    done separately due to the size of this commit.
    All changes to this commit apply to the
    fixtures/node_apps/angular_dotnet path only.

--- fixtures/node_apps/angular_dotnet/ClientApp/src/app/app.module.ts
+++ fixtures/node_apps/angular_dotnet/ClientApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
